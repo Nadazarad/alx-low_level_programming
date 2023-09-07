7be4c99dbecab28e0110b625496cca1300e2783b
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-07 20:04:04 +0300

    n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main- Entry point 
  *
  * Description: C program to print using puts function
  *
@@ -9,8 +9,6 @@
 */  
 int main (void)
 {
-             	puts("\"Programming is like building a multilingual puzzle");
-
-		return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }	
