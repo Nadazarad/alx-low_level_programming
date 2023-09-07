1a1c9364b1b23bae4a1cf86381bcc8b15a3392ad
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-08 01:24:35 +0300

    n

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  *
  * Discription: A C program without printf or puts.
  *
- * Return: Always 1 (success)
+ * Return: Always 0 (success)
 */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar\, 2015-10-19");
-	return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar\, 2015-10-19"\n);
+	return (0);
 }
