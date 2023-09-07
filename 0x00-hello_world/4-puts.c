9fae724e68592778637cbbe767d8bb6bfe4e439f
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-07 19:46:47 +0300

    n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main (){
-     puts("\"Programming is like building a multilingual puzzle"); 
+int main ()
+{
+/**
+ * script to print phrase 
+ *
+ * return to 0 
+ */  
+        	puts("\"Programming is like building a multilingual puzzle");
+
 		return (0);
+
 }	
