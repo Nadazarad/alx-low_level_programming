25be517594fec1e31e1ae676a4e0a5645d0c4cf8
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-07 20:17:25 +0300

    n

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main: a C program
+ *
+ * Description: pront a phrase using printf function
+ *
+ * Return: always (0) for success
+*/
+int main()
+{
+	printf "ith proper grammar, but the outcome is a piece of art/,"
+		Return (0);
+}
