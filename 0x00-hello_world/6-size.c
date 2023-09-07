39d8122a11fb3255843c4c192e2e66560f8b4e38
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-07 22:32:05 +0300

    n

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,9 @@
 
 int main(void)
 {
-	printf("Size of an int: %lu bytes\n";sizeof(intType));
-	printf("Size of a long int: %lu bytes\n";sizeof(longintType));
-	printf("Size of a long long int:%lu bytes\n";sizeof(longlongintType));
-	printf("Size of a float:%lu bytes\n";sizeof(floatType));
+	printf("Size of an int: %lu bytes\n", sizeof(int));
+	printf("Size of a long int: %lu bytes\n", sizeof(long int));
+	printf("Size of a long long int:%lu bytes\n", sizeof(long long int));
+	printf("Size of a float:%lu bytes\n", sizeof(float));
 	return (0);
 }
