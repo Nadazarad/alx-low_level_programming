789e808f9ee5e0ccdd96a2f2f784f10ecbb912c0
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-07 20:52:13 +0300

    n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
- * main- Entry point 
+ * main - Entry point 
  *
  * Description: C program to print using puts function
  *
  * Return: always 0 (success)
 */  
-int main (void)
+
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }	
