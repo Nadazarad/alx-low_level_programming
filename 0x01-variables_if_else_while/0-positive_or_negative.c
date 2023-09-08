e82b923e4b79d748b808020c9f26f9afd9468828
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-08 20:09:31 +0300

    n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,23 +2,24 @@
 #include <time.h>
 #include <stdio.h>
 
-/*main - Entry point
+/* main - Entry point
  *
- * Discreption: A C program positive and negative
+ * Discreption: A C program to validate positive and negative.
  *
- * Return: Always 0
+ * Return: Always 0 (success)
 */
 
 int main(void)
 {
 	int n;
+
 	srand(time(0))		
 		n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive\n",n)
+		printf("%i is positive\n", n);
 	else if (n == 0)
-		printf("%i is zero\n",n)
+		printf("%i is zero\n", n);
 	else if (n < 0)
-		printf("%i is negative\n",n)		
+		printf("%i is negative\n", n);		
 	return (0);
 }
