e76d4dc14a69250ed2c0d87c95a75abf52837bdd
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-12 08:39:34 -0700

    n

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,30 @@
 #include "main.h"
 
 /**
- * print_sign - function that prints the sign of a number
+ * print_sign - print + if n is greater than zero, 0 if n is zerp and - if n is less than zero
  *
  * @c: check the input
  *
- * Return: returns 1 if n is greater than zero
- *         returns 0 if n is zero
- *         returns -1 if n less than zero
+ * Return: returns 1 if +
+ *         returns 0 if  0
+ *         returns -1 if -
 */
 
 int print_sign(int n)
 {
 	if (n > 0)
-		_putchar(n + '43');
-	return (1);
-	else if (n = 0)
-		_putchar(n + '48');
-	return (0);
+	{
+		_putchar('43');
+        	return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('48');
+         	return (0);
+	}
 	else if (n < 0)
-		_putchar(n + '45');
-	return (-1);
+	{
+		_putchar('45');
+        	return (-1);
+	}
 }
