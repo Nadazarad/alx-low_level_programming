aafa2112eca5a55edaa0649de55aa3cd3d133aa7
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-12 12:36:29 -0700

    n

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * void jack_bauer - printa every minute of the day
+*/
+
+void jack_bauer(void);
+{
+	int hr , min
+		for (hr = 0; hr <= 23; hr++)
+		{
+			for (min = 0; min <= 59; min++)
+			{
+				_putchar((hr / 10) + 48));
+				_putchar((hr % 10) + 48);
+				_putchar( : );
+				_putchar((min / 10) + 48);
+				_putchar((min % 10) + 48);
+			}
+		}
