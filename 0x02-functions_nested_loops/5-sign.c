82297387c1c6f58919dd1339aab35ee92085157c
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-12 10:20:26 -0700

    n

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,7 @@
 
 /**
  * print_sign: print + if n is greater than zero,
- *                   0 if n is zero and 
- *                   - if n is less than zero.
+ *                   0 if n is zero and - if n is less than zero.
  *
  * @n: check the input.
  *
@@ -17,16 +16,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-        	return(1);
+        	return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar(48);
-         	return(0);
+         	return (0);
 	}
 	else
 	{
 		_putchar(45);
-        	return(-1);
+        	return (-1);
 	}
 }
