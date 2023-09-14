2eabdf1bbbf175ce71b8b9c7b06ee9af579c8a8b
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-14 09:06:49 -0700

    n

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - prints the numbers from 0 to 9
- *                 followed by a new line
+ *                 using _putchar
  *
  * Return: Always 0 (success)
 */
@@ -13,7 +13,7 @@ void print_numbers(void)
 
 	do {
 		_putchar(num + 48);
-		num++
+		num++;
 	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
