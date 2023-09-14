769a7d1648213ddb7d801bfc36c4643086521d2b
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-14 10:04:51 -0700

    n

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,17 +12,12 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	/**
-	 * for a leap year it must by divisible by 4
-	 * or divisible by 100 and 400
-	*/
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -30,7 +25,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year)
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
