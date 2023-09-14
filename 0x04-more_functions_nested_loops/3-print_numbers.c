97091f194a580879e8c881444fe52d9929d49ffa
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-14 07:11:52 -0700

    n

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers from 0 to 9
+ *                 followed by a new line
+ *
+ * @i: the number
+ *
+ * return: Always 0
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	if (i >= 48 && i <= 57)
+		_putchar(i);
+	_putchar('\n');
+}
