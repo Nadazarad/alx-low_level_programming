cd05801a0ea3e40e0b5469ae343117209abf371d
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-14 11:31:38 -0700

    n

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,15 +8,18 @@
 
 void print_diagonal(int n)
 {
-	int dia;
+	int dia, space;
 
-	if (n == 0)
+	if (n <= 0)
 		_putchar('\n');
 	else
 	{
 		for (dia = 1; dia <= n; dia++)
+		{
+			for (space = 1; space <= dia; space++)
 			_putchar(' ');
-		_putchar('\');
+		_putchar('92');
 		_putchar('\n');
+		}
 	}
 }
