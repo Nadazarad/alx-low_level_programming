80a9d069ca84170c83ecba3acbf031c3ab47b66a
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-14 12:05:05 -0700

    n

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using #
+ *
+ * @size: size of the square
+*/
+
+void print_square(int size)
+{
+	int len, width;
+
+	for (len = 1; len <= size; len++)
+	{
+		for (width = 1; width <= size; width++)
+		_putchar(35);
+		_putchar('\n');
+	}
+}
+
