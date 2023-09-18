a71fe7cc34be0c4d6df60326742aca19d9f8dd0e
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-18 10:36:18 -0700

    n

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#includ "main.h"
+
+/**
+ * print_rev - prints reversed string, folowed by a new line
+ * @s: pointer to the string to print
+ * Return: void
+*/
+
+void print_rev(char *s);
+{
+	int i = 0;
+
+	while (%[i])
+		i++;
+
+	while (i--)
+	{
+		putchar(%[i]);
+	}
+	_putchar{'\n'};
+}
