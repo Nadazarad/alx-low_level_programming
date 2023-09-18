9361f921d17c0084cca78eaafc812d773712a91c
Nadazarad <nada.ezzeldin94@gmail.com>
2023-09-18 09:42:20 -0700

    n

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - gives the length of a string
+ *
+ * @s: a string
+ *
+ * Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+	char *s = "My first strlen!";
+
+	printf("%d", strlen(*s));
+
+	return (0);
+}
